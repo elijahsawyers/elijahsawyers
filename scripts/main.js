a8625085a673dc179bb6a3b36fd29c81e7d3187e
Elijah Sawyers <elijahsawyers@gmail.com>
2019-03-31 20:43:12 -0500

    Changes to the landing content

--- scripts/main.js
+++ scripts/main.js
@@ -13,8 +13,17 @@ function setStyles() {
 
     // Grab all landing content.
     let landingContainer = document.getElementById("landing-container");
-    let caption = document.getElementById("landing-caption");
+    let landingCaption = document.getElementById("landing-caption");
+    let img = document.getElementById("landing-img");
 
-    // Center the caption vertically.
-    caption.style.top = ((landingContainer.clientHeight/2) - (caption.clientHeight/2)) + "px";
+    // Set caption vertical offset.
+    landingCaption.style.top = (((landingContainer.clientHeight/2) - (landingCaption.clientHeight/2)) * 0.75) + "px";
+
+    // If the window is less than Bootstrap lg, change the image.
+    if (window.innerWidth < 997) {
+        console.log("Here");
+        img.src = "../images/Me Mobile.jpg";
+    } else {
+        img.src = "../images/Me Desktop.jpg"
+    }
 }
