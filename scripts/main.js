34b1d3f4d718ea9ef531afdc00ad15bf2bf4c52f
Elijah Sawyers <elijahsawyers@gmail.com>
2019-04-06 23:04:55 -0500

    Finished landing

--- scripts/main.js
+++ scripts/main.js
@@ -15,9 +15,7 @@ function setStyles() {
     let landingContainer = document.getElementById("landing-container");
     let landingCaption = document.getElementById("landing-caption");
     let img = document.getElementById("landing-img");
-
-    // Set caption vertical offset.
-    landingCaption.style.top = (((landingContainer.clientHeight/2) - (landingCaption.clientHeight/2)) * 0.75) + "px";
+    let grey = document.getElementById("landing-grey-bg");
 
     // If the window is less than Bootstrap lg, change the image.
     if (window.innerWidth < 997) {
@@ -25,5 +23,12 @@ function setStyles() {
         img.src = "../images/Me Mobile.jpg";
     } else {
         img.src = "../images/Me Desktop.jpg"
+    }
+
+    // Wait for the image to load before styling, due to container size change.
+    img.onload = (e) => {
+        // Set caption vertical offset based on the container size.
+        landingCaption.style.top = (((landingContainer.clientHeight/2) - (landingCaption.clientHeight/2)) * 0.75) + "px";
+        grey.style.display = "block";
     }
 }
