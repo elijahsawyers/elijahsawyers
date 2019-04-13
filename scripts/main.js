f935bfa505f81564205946a6507bc354082cc49a
Elijah Sawyers <elijahsawyers@gmail.com>
2019-04-12 20:20:26 -0500

    Working on landing content

--- scripts/main.js
+++ scripts/main.js
@@ -2,33 +2,10 @@ window.onload = (e) => {
     setStyles();
 }
 
-window.onresize = (e) => {
-    setStyles();
-}
-
 function setStyles() {
-    //====================================================================================================
-    // Landing Content Styles
-    //====================================================================================================
-
-    // Grab all landing content.
-    let landingContainer = document.getElementById("landing-container");
-    let landingCaption = document.getElementById("landing-caption");
-    let img = document.getElementById("landing-img");
-    let grey = document.getElementById("landing-grey-bg");
-
-    // If the window is less than Bootstrap lg, change the image.
-    if (window.innerWidth < 997) {
-        console.log("Here");
-        img.src = "../images/Me Mobile.jpg";
-    } else {
-        img.src = "../images/Me Desktop.jpg"
-    }
-
-    // Wait for the image to load before styling, due to container size change.
+    let img = new Image();
+    img.src = "../images/Me\ Desktop.jpg";
     img.onload = (e) => {
-        // Set caption vertical offset based on the container size.
-        landingCaption.style.top = (((landingContainer.clientHeight/2) - (landingCaption.clientHeight/2)) * 0.75) + "px";
-        grey.style.display = "block";
+        document.getElementsByTagName("html")[0].classList.add("show-html");
     }
 }
