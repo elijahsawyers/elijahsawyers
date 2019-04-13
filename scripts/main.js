55634e806a9e8a70f33c95de19560954c7d1775d
Elijah Sawyers <elijahsawyers@gmail.com>
2019-04-13 17:59:41 -0500

    Fixed mobile phone layout issue

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,19 @@ window.onload = (e) => {
 }
 
 function setStyles() {
+    // Change the landing image src and size if it's a mobile phone.
+    let landingContent = document.getElementById("landing-content");
+    if (window.innerWidth <= 768) {
+        console.log("here");
+        landingContent.style.backgroundImage = "url(\"../../images/Me\ Mobile.jpg\")"
+    }
+    landingContent.style.height = (landingContent.clientHeight - document.getElementById("navbar").clientHeight) + "px"
+
+    // When the landing image loads, fade in the content.
     let img = new Image();
     img.src = "../images/Me\ Desktop.jpg";
     img.onload = (e) => {
         document.getElementsByTagName("html")[0].classList.add("show-html");
-    }
+    };
+    delete img;
 }
