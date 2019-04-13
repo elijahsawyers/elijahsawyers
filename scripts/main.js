0dac9fde42127cf93eb232066d2f208826f6e436
Elijah Sawyers <elijahsawyers@gmail.com>
2019-04-13 18:17:14 -0500

    Fixed mobile phone layout issue

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,9 @@ function setStyles() {
     // Change the landing image src and size if it's a mobile phone.
     let landingContent = document.getElementById("landing-content");
     if (window.innerWidth <= 768) {
-        console.log("here");
         landingContent.style.backgroundImage = "url(\"../../images/Me\ Mobile.jpg\")"
+        landingContent.style.height = (landingContent.clientHeight - document.getElementById("navbar").clientHeight) + "px"
     }
-    landingContent.style.height = (landingContent.clientHeight - document.getElementById("navbar").clientHeight) + "px"
 
     // When the landing image loads, fade in the content.
     let img = new Image();
