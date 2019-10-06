b79ee6765f25d7b2098ed2e2f4c96ebc99816842
Elijah Sawyers <elijahsawyers@gmail.com>
2019-10-05 23:38:02 -0500

    Removed skillset and fixed position image

--- js/main.js
+++ js/main.js
@@ -1,68 +1,34 @@
+/**
+ * Author: Elijah Sawyers
+ * Email: elijahsawyers@gmail.com
+ */
+
+'use strict';
+
 (function($) {
-	"use strict"
 
-	///////////////////////////
-	// Preloader
-	$(window).on('load', function() {
-		$("#preloader").delay(600).fadeOut();
-	});
-
-	///////////////////////////
-	// Scrollspy
-	$('body').scrollspy({
-		target: '#nav',
-		offset: $(window).height() / 2
-	});
-
-	///////////////////////////
-	// Smooth scroll
+	/* -- Smooth scroll on nav click -- */
 	$("#nav .main-nav a[href^='#']").on('click', function(e) {
 		e.preventDefault();
-		var hash = this.hash;
+		const hash = this.hash;
 		$('html, body').animate({
 			scrollTop: $(this.hash).offset().top
 		}, 600);
 	});
 
-	$('#back-to-top').on('click', function(){
-		$('body,html').animate({
-			scrollTop: 0
-		}, 600);
-	});
-
-	///////////////////////////
-	// Btn nav collapse
-	$('#nav .nav-collapse').on('click', function() {
-		$('#nav').toggleClass('open');
-	});
-
-	///////////////////////////
-	// Mobile dropdown
+	/* -- Mobile dropdown -- */
 	$('.has-dropdown a').on('click', function() {
 		$(this).parent().toggleClass('open-drop');
 	});
 
-	///////////////////////////
-	// On Scroll
+	/* -- Fixed nav -- */
 	$(window).on('scroll', function() {
-		var wScroll = $(this).scrollTop();
-
-		// Fixed nav
-		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
-
-		// Back To Top Appear
-		wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
+		const scrollY = $(this).scrollTop();
+	
+		scrollY > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
 	});
 
-	///////////////////////////
-	// magnificPopup
-	$('.work').magnificPopup({
-		delegate: '.lightbox',
-		type: 'image'
-	});
-
-	///////////////////////////
-	// Owl Carousel
+	/* -- Owl Carousel -- */
 	$('#about-slider').owlCarousel({
 		items:1,
 		loop:true,
@@ -72,22 +38,6 @@
 		dots : true,
 		autoplay : true,
 		animateOut: 'fadeOut'
-	});
-
-	$('#testimonial-slider').owlCarousel({
-		loop:true,
-		margin:15,
-		dots : true,
-		nav: false,
-		autoplay : true,
-		responsive:{
-			0: {
-				items:1
-			},
-			992:{
-				items:2
-			}
-		}
 	});
 
 })(jQuery);
